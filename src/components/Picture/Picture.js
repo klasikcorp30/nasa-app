a24d60e2ecfdf47ffe258aa9de8ceab442709d1d
klasikcorp30 <32335856+klasikcorp30@users.noreply.github.com>
2018-11-02 01:38:17 -0400

    iframe

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -5,11 +5,13 @@ class Picture extends Component {
     render() {
         let url = String(this.props.url);
         return (
-        <div className="text-center embed-responsive embed-responsive-16by9">
+        <div className="text-center">
             {url.includes('.jpg') ?
             <img className='rounded h-50 w-50' src={this.props.url} alt='APOD'/>
             :
-            <iframe title='nasa' className='rounded mt-5' width="600px" height="500px"  src={this.props.url} frameborder="0" allowfullscreen="true"></iframe>
+			<div className="embed-responsive embed-responsive-16by9">
+				<iframe title='nasa' className='rounded mt-5' width="600px" height="500px"  src={this.props.url} frameborder="0" allowfullscreen="true"></iframe>
+			</div>
             }
         </div>
         );
