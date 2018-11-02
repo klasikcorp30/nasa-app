04d58d2f776d24f8006e1e818cca39caa9be748e
klasikcorp30 <32335856+klasikcorp30@users.noreply.github.com>
2018-11-02 00:26:04 -0400

    iframe responsive

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -5,7 +5,7 @@ class Picture extends Component {
     render() {
         let url = String(this.props.url);
         return (
-        <div className="text-center">
+        <div className="text-center embed-responsive embed-responsive-16by9">
             {url.includes('.jpg') ?
             <img className='rounded h-50 w-50' src={this.props.url} alt='APOD'/>
             :
